6c0479693367c08f431d23d89afe777ff4b7750d
Marcin Wiśniewski <marcinwisniewski707@gmail.com>
2025-03-16 22:43:52 +0100

    Implemented PGN loader for analysis engine

--- components/Analysis.tsx
+++ components/Analysis.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Sidebar from "./Sidebar";
+import Sidebar from "./Rightbar";
 import dynamic from "next/dynamic";
 import "../app/globals.css";
 import React from "react";
@@ -7,13 +7,16 @@ import React from "react";
 const Chessboard = dynamic(() => import("react-chessboard").then((mod) => mod.Chessboard), { ssr: false });
 
 export default function Analysis() {
-    const [board, setBoard] = React.useState<string[]>([])
+    const [board, setBoard] = React.useState<string>('start')
     return (
     <div className="flex justify-center items-center">
         <Chessboard boardWidth={560} customBoardStyle={{
             borderRadius: "10px"
-        }}/>
-        <Sidebar setBoard={setBoard}/>
+            
+        }}
+        position={board}
+        />
+        <Sidebar setBoard={setBoard} board={board}/>
     </div>
 
     )
